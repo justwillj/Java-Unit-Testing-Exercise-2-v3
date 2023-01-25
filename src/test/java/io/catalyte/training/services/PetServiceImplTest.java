ca612ea20d145070d056a405962e0649fd51ef7c
Justin Williams <williams.jman@gmail.com>
2023-01-24 23:34:00 -0500

    Added PetServiceTest class

---
+++ src/test/java/io/catalyte/training/services/PetServiceImplTest.java
@@ -0,0 +1,17 @@
+package io.catalyte.training.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetServiceImplTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void queryPets() {
+  }
+}
